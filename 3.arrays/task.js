e0df6fbf7b54184e261be0a2d5c3997361f99a1d
Neto test <2001107@mail.ru>
2024-08-01 19:25:02 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	let comp = arr1.every((element, index) => element === arr2[index]);
+	return comp;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const filteredUsers = users.filter(user => user.gender === gender);
+	const totalAge = filteredUsers.reduce((acc, user) => acc + user.age, 0);
+	const averageAge = filteredUsers.length > 0 ? totalAge / filteredUsers.length : 0;
+	return averageAge;
 }
